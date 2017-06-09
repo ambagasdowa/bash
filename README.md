9b9b4e6e09519899d9af684daa71b092e9d5b8ec
ambagasdowa <baizabal.jesus@gmail.com>
2017-06-09 12:16:31 -0500

    Add extracting script

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # bash
 ## multiple bash scripts
 
-
-
 ### battery_osd_monitor
 
 Just a simple battery monitor using **osd** and / or *notify-send*
@@ -31,5 +29,35 @@ $ sh battery_osd_monitor.sh &
 
 TODO
 > install instructions for starting as daemon
+
+
+### extract
+
+extractor with **passwords** list
+
+#### Dependencies on debian:
+* 7zip-full *For rar support*
+
+Edit this lines with your passwords and your extraction path
+
+```javascript
+ PASSWORDS=$'pass1\npass2\npass3' # whit your passwords
+ filepath="/path/to/data/inflating" #whit your extraction path
+
+```
+then you are ready to go
+```javascript
+  $ cd /to/path/of/zipped/files
+  #and run the script
+  $ path/to/extract.sh
+```
+
+### Version updates
+0.0.1
+>firts release
+
+
+TODO
+> set working directory 
 
 ### more scripts ... soon
---
+++ extract.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# TODO read passwords from a database and read a var to set the extraction path
+IFS=$'\n'  # make newlines the only separator
+LIST=$(find . -type f -name '*.*' | sed 's|.*\.||' | sort -u)
+PASSWORDS=$'www.mega-dvdrip.com\nvsacaba\nwww.bajarpelisgratis.com'
+filepath="/path/to/data/inflating"
+status_file=""
+
+
+	for i in $LIST; do
+	echo "Archivo=$i"
+	declare -a files=$(find . -iname "*.$i" -type f -exec basename {} \;)
+	echo "${files[@]}"
+
+	      	for pass in $PASSWORDS; do
+
+								for infile in ${files[@]} ; do
+									echo
+									echo "Testing file ${infile} with password ${pass}"
+									7z -p"$pass" t "${infile}"
+									status_file=$?
+									if [ "${status_file}" -eq 0 ]; then
+										if [[  ${#files[@]} > 0 ]]; then
+											echo "Password ${pass} ok then extracting ...."
+											7z -y -aoa -p"$pass" -o"$filepath" e ${infile}
+											echo
+											echo "=====================  Success ====================="
+											echo
+											declare -a patter=( ${files[@]/$infile/} )
+											files=("${patter[@]}")
+										else
+											echo
+										echo "=================  files extracted ==================="
+											echo
+										fi
+							    else
+										echo "$pass is not working with ${infile}"
+							      echo
+							      echo "========================  Fail ======================="
+										echo
+							    fi
+								done
+					done
+	done;
