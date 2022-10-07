de0b8402675de7db1d78f3d158933c1c8f0e19b7
Legion <baizabal.jesus@gmail.com>
2022-10-07 11:07:02 -0500

    Working in osd

--- battery_osd_monitor.sh
+++ battery_osd_monitor.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
-echo 'Initializing battery-monitor ...' | aosd_cat -p 6 -R green
+# install xosd-bin on debian
+#echo 'Initializing battery-monitor ...' | aosd_cat -p 6 -R green
+echo 'Initializing battery-monitor ...' | osd_cat -A center -p bottom -f -*-*-bold-*-*-*-36-120-*-*-*-*-*-* -cgreen -s 5
 
 while true
 do
  #   battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
-    battery_level=`acpitool --battery | grep 'All batteries' | cut -d':' -f 2 | cut -f 1 -d '.'`
-    remain=`acpitool --battery | grep 'All batteries' | cut -d',' -f 2`
+   # battery_level=`acpitool --battery | grep 'All batteries' | cut -d':' -f 2 | cut -f 1 -d '.'`
+    battery_level=`acpitool --battery | grep 'Remaining capacity' | cut -d':' -f 2 | cut -f 2 -d ',' | cut -f 1 -d '.'`
+    #remain=`acpitool --battery | grep 'All batteries' | cut -d',' -f 2`
+    remain=`acpitool --battery | grep 'Remaining capacity' | cut -d',' -f 3`
     chck=$(expr length "$battery_level") # when acpi -b is null is because no battery detect or installed 
+
+#    echo $battery_level;
+#    echo $remain;
+#    echo $chck;
+
+
 if [ $chck != 0 ]; then
   if [ $battery_level -eq 100 ]; then
   	if [ `acpi -a | awk '{print $3}'` = "on-line" ];then
   		# notify-send "Battery charging above 99%. Please unplug your AC adapter!" "Charging: ${battery_level}%"
-      echo "Battery charging above 100%. Please unplug your AC adapter!" "Charging: ${battery_level}%" | aosd_cat -p 6 -R green
+          echo "Battery charging above 100%. Please unplug your AC adapter!" "Charging: ${battery_level}%" | aosd_cat -p 6 -R green
   	fi
   elif [ $battery_level -le 10 ];then
   	if [ `acpi -a | awk '{print $3}'` = "off-line" ];then
